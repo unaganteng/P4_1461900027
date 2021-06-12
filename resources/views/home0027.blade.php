81d25246acb3988f35c42e3006e82d9f588dcee2
unaganteng <rismasykes10@gmail.com>
2021-06-12 13:28:02 +0700

    selesai

--- resources/views/home0027.blade.php
+++ resources/views/home0027.blade.php
@@ -6,12 +6,10 @@
 <body>
     <h2>Kegiatan 4</h2>
     <h3>1461900027</h3>
-    <p>Cari Buku :</p>
-        <form action="/buku/cari" method="GET">
-        <input type="text" name="lihat" placeholder="Masukan disini .." value="{{ old('cari') }}">
-        <input type="submit" value="CARI">
+
         </form>
-        <a href="/export"> export </a>
+        <a href="/export"> EXPORT DISINI </a>
+        <p></p>
     <table border="1" style="background-color:Tomato; width:100%">
     
 <tr>
